f562951d6d6e023f4509690bc26e6a7ea643c1a7
Jack Trinh <46969960+jacktrinity@users.noreply.github.com>
2021-04-02 20:02:40 -0700

    Create Probability-Occurrence.py

---
+++ Probability-Occurrence.py
@@ -0,0 +1,27 @@
+"""
+Probability of an event occuring at least once.
+
+Given a probability of an event occuring and n amount of trials,
+find the probability that it will occur at least once.
+    
+i.e. What is the probability of getting head, at least once, if given 4 coins?
+p(at least one head | 4 trials) = 0.9375
+
+CodeSkulptor: https://py3.codeskulptor.org/#user306_qjCERiGxA3_0.py
+Written in Python 3
+"""
+
+def event_outcome(p_chance, n):
+    """
+    Take two arguments
+    p_chance is the probability of an event occuring
+    n amount of given trials
+    
+    return a float number of the probability of the outcome
+    """
+    whole_space = float(1)
+    p_not_occuring = (1 - p_chance) ** n
+    
+    # p(occuring at least once) = p(whole space) - p(not occuring)
+    return whole_space - p_not_occuring
+    
